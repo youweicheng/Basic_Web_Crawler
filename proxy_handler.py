553d67ed1fb4e824960b620c4518ffb481835073
weicheng yu <67583536+youweicheng@users.noreply.github.com>
2022-03-10 22:39:34 +0800

    Update proxy_handler.py

--- proxy_handler.py
+++ proxy_handler.py
@@ -3,10 +3,24 @@ import urllib.request
 def create_proxy_handler():
   
   url = "https://en.wikipedia.org/wiki/Adriana_Lima"
-  
+  # add proxy
   proxy = {
-    "http"
-  
-  
+    # free version : "http":"120.77.249.46:8080"
+    # paid version : "http":"username:pwd@192.168.12.11:8080"
+    "http":""
+ 
   }
+  # proxy handler
+  proxy_handler = urllib.request.ProxyHandler(proxy)
+  # create opener
+  opener = urllib.request.build_opener(proxy_handler)
+  # get response with proxy ip
+  response = opener.open(url)
+  
+  data = response.read().decode("utf-8")
+  
+  with open("proxy_handler.html", "w", encoding="utf-8") as f:
+    f.write(data)
+    
+create_proxy_handler()
     
